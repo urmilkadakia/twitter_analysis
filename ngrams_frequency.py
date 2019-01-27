9ceda6b3cbe77c634479816738b42610c944d0df
Urmil Kadakia <urmil.kadakia@gmail.com>
2019-01-27 16:26:45 -0500

    modified the code to generate the conbined unigram frequency

--- ngrams_frequency.py
+++ ngrams_frequency.py
@@ -15,6 +15,7 @@ import pandas as pd
 from config import parse_args2
 import os
 import time
+import glob
 
 
 def ngram_frequency_dist(inputfile, outputfilepath, n=1):
@@ -55,46 +56,46 @@ def ngram_frequency_dist(inputfile, outputfilepath, n=1):
             writer.writerow(item)
 
 
-def daily_unigram_collector(inputfile, outputfile):
-    with open(inputfile, "r") as f:
-        json_list = json.load(f)
+def daily_unigram_collector(inputfilepath, outputfile):
+    for file in glob.glob(os.path.join(inputfilepath, 'output_*2019.txt')):
+        with open(file, "r") as f:
+            json_list = json.load(f)
+        text = ''
+        for user in json_list:
+            text += user['description'].lower() + " "
 
-    text = ''
-    for user in json_list:
-        text += user['description'].lower() + " "
+        # get rid of punctuation (except periods!)
+        punctuation_no_period = "[" + re.sub("\.", "", string.punctuation) + "]"
+        text = re.sub(punctuation_no_period, "", text)
 
-    # get rid of punctuation (except periods!)
-    punctuation_no_period = "[" + re.sub("\.", "", string.punctuation) + "]"
-    text = re.sub(punctuation_no_period, "", text)
+        # Splits the sentences into words
+        tknzr = TweetTokenizer(strip_handles=True, reduce_len=True)
+        tokens = tknzr.tokenize(text)
+        # tokens = nltk.word_tokenize(text)
 
-    # Splits the sentences into words
-    tknzr = TweetTokenizer(strip_handles=True, reduce_len=True)
-    tokens = tknzr.tokenize(text)
-    # tokens = nltk.word_tokenize(text)
+        ngrams_list = list(ngrams(tokens, 1))
+        # get the frequency of each ngram in our corpus
+        ngram_freq = collections.Counter(ngrams_list)
+        ngram_freq = ngram_freq.most_common()
 
-    ngrams_list = list(ngrams(tokens, 1))
-    # get the frequency of each ngram in our corpus
-    ngram_freq = collections.Counter(ngrams_list)
-    ngram_freq = ngram_freq.most_common()
+        # Creating the new row to add to the daily collector file
+        new_row1 = {}
+        # Extracting the Date from the filename
+        new_row1['Date'] = re.findall(r'[0-9]{2}_[0-9]{2}_[0-9]{4}', file)[0]
+        for item, val in ngram_freq:
+            new_row1[item[0]] = [val]
+        new_row = pd.DataFrame(new_row1)
+        # Setting the index key to the Date
+        # new_row.set_index('Date', inplace=True)
 
-    # Creating the new row to add to the daily collector file
-    new_row1 = {}
-    new_row1['Date'] = [time.strftime("%Y_%m_%d")]
-    for item, val in ngram_freq:
-        new_row1[item[0]] = [val]
-    new_row = pd.DataFrame(new_row1)
-    # Setting the index key to the Date
-    new_row.set_index('Date', inplace=True)
-
-    # Checking the file exist or not
-    # If no then generate a new one or append the line at the end of the file
-    if not os.path.exists(outputfile + 'combine.csv'):
-        unigram_conbined = new_row
-    else:
-        unigram_original = pd.read_csv(outputfile + 'combine.csv', index_col=0)
-        unigram_conbined = pd.concat([unigram_original, new_row], sort=False, ignore_index=True)
-        unigram_conbined.drop(['0501201'], inplace=True, axis=1)
-    unigram_conbined.to_csv(outputfile + 'combine.csv')
+        # Checking the file exist or not
+        # If no then generate a new one or append the line at the end of the file
+        if not os.path.exists(outputfile):
+            unigram_combined = new_row
+        else:
+            unigram_original = pd.read_csv(outputfile, index_col=0)
+            unigram_combined = pd.concat([unigram_original, new_row], sort=False, ignore_index=True, axis=0)
+        unigram_combined.to_csv(outputfile)
 
 
 def gen_histogram(inputfile, outputfilepath):
@@ -131,9 +132,9 @@ def gen_histogram1(inputfile, outputfilepath, n=1):
     ydata = data.ix[:, -1]
 
     # Plotting the top 50 ngrams of the given file
-    plt.bar(xdata[:50], ydata[:50])
+    plt.bar(xdata[:200], ydata[:200])
     plt.xlabel('character length')
-    plt.xticks(xdata[:50], xdata[:50], rotation='vertical')
+    plt.xticks(xdata[:200], xdata[:200], rotation='vertical')
     plt.ylabel('Frequency')
     plt.title('Description character length distribution')
     plt.savefig(outputfilepath + "ngram_frequency.png")
@@ -144,8 +145,8 @@ def main():
     args = parse_args2()
     # ngram_frequency_dist(args.inputfile, args.outputfile, args.n)
 
-    # daily_unigram_collector(args.inputfile, args.outputfile)
-    gen_histogram1(args.inputfile, args.outputfile, n=1)
+    daily_unigram_collector(args.inputfile, args.outputfile)
+    # gen_histogram1(args.inputfile, args.outputfile, n=1)
 
 
 if __name__ == "__main__":
