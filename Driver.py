b7c96991e9bc998e430590f859ad5da0d953f8b9
urmilkadakia <urmil.kadakia@gmail.com>
2019-03-11 01:30:49 -0400

    Function to generate alloy matrix

--- Driver.py
+++ Driver.py
@@ -1,7 +1,8 @@
 from util import parse_args
-from ngram_methods import daily_ngram_collector, changing_ngram, ngram_frequency_dist, get_locations, \
-    ngram_histogram, char_length_histogram, ngram_adjacency_matrix
+from ngram_methods import daily_ngram_collector, changing_ngram, ngram_frequency_dist, ngram_histogram, \
+    char_length_histogram, ngram_adjacency_matrix, ngram_alloy_matrix
 
+from account_methods import get_locations, bot_or_not, entities_count_difference, description_change_frequency
 
 def main():
 
@@ -12,7 +13,10 @@ def main():
     # get_locations(args.input_file1, args.input_file2, args.output_file)
     # ngram_histogram(args.input_file1, args.output_file, args.n, args.cutoff_freq)
     # char_length_histogram(args.input_file1, args.output_file)
-    ngram_adjacency_matrix(args.input_file1, args.output_file, args.n, args.cutoff_freq)
+    # ngram_adjacency_matrix(args.input_file1, args.output_file, args.n, args.cutoff_freq)
+    # entities_count_difference(args.input_file1, args.input_file2, args.output_file)
+    # description_change_frequency(args.input_file1, args.output_file)
+    ngram_alloy_matrix(args.input_file1, args.input_file2, args.output_file, args.n)
 
 
 if __name__ == "__main__":
